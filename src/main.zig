27eecb32f8a2e118c8768e6250506c80d80bf24f
Elijah M. Immer <me@eimmer.me>
2024-03-06 21:30:02 -0800

    Run command and errors

--- src/main.zig
+++ src/main.zig
@@ -1,50 +1,102 @@
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
+    const stdout_file = io.getStdOut();
+    const stdout = stdout_file.writer();
     // Everything I print is direct, no need for buffer.
 
-    const stdin = std.io.getStdIn().reader();
+    const stdin = io.getStdIn().reader();
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
+    var gpalloc = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpalloc.deinit();
 
-    const alloc = gpa.allocator();
+    const allocator = gpalloc.allocator();
 
-    try stdout.print("{s}", .{HELP_MESSAGE});
+    var history_array = ArrayList([]const u8).init(allocator);
+    defer history_array.deinit();
 
+    var last_status: u32 = 0;
+
+    var config = tty.detectConfig(stdout_file);
     while (true) {
-        try stdout.print("hsh$ ", .{});
+        try config.setColor(stdout_file, Color.cyan);
+        try config.setColor(stdout_file, Color.bold);
 
-        const buf = (try stdin.readUntilDelimiterOrEofAlloc(alloc, '\n', MAX_LINE_BUFFER)).?;
-        defer alloc.free(buf);
+        try stdout.print("hsh {}$ ", .{last_status});
 
-        if (ascii.eqlIgnoreCase(buf, "exit")) {
+        try config.setColor(stdout_file, Color.reset);
+
+        const buf = try stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', MAX_LINE_BUFFER) orelse break;
+        defer allocator.free(buf);
+
+        const command = mem.trim(u8, buf, &ascii.whitespace);
+
+        if (ascii.eqlIgnoreCase(command, "exit")) {
+            try stdout.print("exiting...\n", .{});
             break;
         }
-        if (ascii.eqlIgnoreCase(buf, "help")) {
-            try stdout.print("{s}", .{HELP_MESSAGE});
+
+        if (ascii.eqlIgnoreCase(command, "help")) {
+            try stdout.print("{s}\n", .{HELP_MESSAGE});
             continue;
         }
 
-        _ =  try tokenize(alloc, buf);
+        var arg_arr = try tokenize(allocator, command);
+        defer arg_arr.deinit();
+
+        last_status = run_command(allocator, arg_arr.items) catch |err| rcmd: {
+            try stdout.print("failed to run command: {}\n", .{err});
+            break :rcmd 0;
+        };
     }
 
-    std.process.cleanExit();
+    process.cleanExit();
 }
 
-pub fn tokenize(allocator: Allocator, input: []const u8) !ArrayList([] u8) {
-    const trimed = mem.trim(u8, input, &ascii.whitespace);
-
-    var itr = mem.tokenize(u8, trimed, &ascii.whitespace);
-    var arr = ArrayList([]const u8).init(allocator);
+//// Tokenize a string into the command's argv
+//// TODO: Implement it to follow quotes and other marks like pipes.
+pub fn tokenize(allocator: Allocator, input: []const u8) Allocator.Error!ArrayList([]const u8) {
+    var itr = mem.tokenize(u8, input, &ascii.whitespace);
+    var arr = try ArrayList([]const u8).initCapacity(allocator, 16);
 
     while (itr.next()) |val| {
         try arr.append(val);
-        std.debug.print("{s}\n", .{val});
     }
-
 
     return arr;
 }
+
+//// Run a command given the argv
+//// TODO: Implement piping for input and output.
+pub fn run_command(allocator: Allocator, argv: []const []const u8) anyerror!u32 {
+    assert(argv.len > 0);
+    //var child = ChildProcess.init(argv, allocator);
+
+    //try child.spawn();
+
+    //_ = try child.wait();
+
+    _ = &allocator;
+    _ = &argv;
+
+    const pid = try os.fork();
+
+    if (pid == 0) {
+        var err = process.execv(allocator, argv);
+
+        const stdout = io.getStdOut().writer();
+
+        try stdout.print("{s}: {s}\n", .{ argv[0], switch (err) {
+            error.FileNotFound => "command not found",
+            else => @errorName(err),
+        } });
+
+        os.exit(1);
+    }
+
+    const res = os.waitpid(pid, 0);
+    return res.status;
+}
+
+//var current_process: ?pid_t = null;
 
 const CMD_HELP_MESSAGE =
     \\
@@ -58,22 +110,27 @@ const CMD_HELP_MESSAGE =
     \\
 ;
 
-
 const HELP_MESSAGE =
     \\
     \\help: prints this message
     \\exit: exits the shell
     \\
-    \\
 ;
 
 //// The max length of the buffer allocated for reading from the stdin
-//// A far more than reasonable max buffer size
+////    A far more than reasonable max buffer size
 const MAX_LINE_BUFFER = 1 << 16;
 
 const std = @import("std");
 const ascii = std.ascii;
 const mem = std.mem;
+const os = std.os;
+const process = std.process;
+const io = std.io;
+const tty = io.tty;
 
 const Allocator = mem.Allocator;
 const ArrayList = std.ArrayList;
+const ChildProcess = std.ChildProcess;
+const Color = io.tty.Color;
+const assert = std.debug.assert;
